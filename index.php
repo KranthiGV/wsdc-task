9156612b9e3e6dddac2db264f7a908930cf61d6b
Kranthi <kranthi.gv@hotmail.com>
2015-09-02 16:58:40 +0530

    Added links for login and register

--- index.php
+++ index.php
@@ -6,7 +6,15 @@ if(Session::exists('home')){
 	echo Session::flash('home');
 }
 
+/*
 if(Input::get('u')){
 	$db =  DB::getInstance()->get('users', array('username', '=', Input::get('u')));
 	var_dump($db);
 }
+*/
+?>
+<ul>
+<a href="login.php"><li>Log in</li></a>
+
+<a href="register.php"><li>Register</li></a>
+</ul>
